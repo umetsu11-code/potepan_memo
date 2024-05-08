007ccf1832fe9d0ffd197c60070e6e3b290342aa
umetsu sunao <s.umetsu11@gmail.com>
2024-05-09 03:17:04 +0900

    ctrl+lにて動作が終了するよう改修いたしました。何卒ご確認をお願いいたします。

--- memo.rb
+++ memo.rb
@@ -6,49 +6,50 @@ memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換�
 
 # if文を使用して続きを作成していきましょう。
 # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
-if memo_type == 1
-  puts "新規でメモを作成します"
-  puts "メモの内容を入力してください"
-  memo_content = gets.chomp
+    if memo_type == 1
+        puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
+        file_name = gets.chomp
+        
+        puts"メモの内容を記入して下さい。Ctrl+Dで保存します。"
+        imput_memo = STDIN.read
+        memo = imput_memo.chomp
+        
+        CSV.open("#{file_name}.csv", "w") do |csv|
+        csv << [memo]
+        end
 
-  # CSVの読み込みを実行する処理
-  CSV.open("memos.csv", "a") do |csv|
-    csv << [memo_content]
-  end
-
-  puts "メモが作成されました"
-
-elsif memo_type == 2
-  puts "既存のメモを編集します"
-
-  # CSVファイルからメモを読み込み、表示する
-  memos = CSV.read("memos.csv")
-  memos.each_with_index do |memo, index|
-    puts "#{index + 1}: #{memo[0]}"
-  end
-
-  puts "編集したいメモの番号を入力してください："
-  edit_index = gets.to_i - 1 # 編集したいメモの番号を取得し、配列のインデックスに変換する
-
-  if edit_index >= 0 && edit_index < memos.length
-    puts "現在の内容：#{memos[edit_index][0]}"
-    puts "新しい内容を入力してください："
-    new_content = gets.chomp # 新しい内容をユーザーから受け取る
-
-    # メモを更新する
-    memos[edit_index][0] = new_content
-
-    # 更新したメモをCSVファイルに書き込む
-    CSV.open("memos.csv", "w") do |csv|
-      memos.each do |memo|
-        csv << memo
+      elsif memo_type == 2
+        puts "既存のメモを編集します"
+      
+        # CSVファイルからメモを読み込み、表示する
+        puts "編集したいメモのファイル名を拡張子を除いて入力してください："
+        file_name = gets.chomp
+        memos = CSV.read("#{file_name}.csv")
+        memos.each_with_index do |memo, index|
+          puts "#{index + 1}: #{memo[0]}"
+        end
+      
+        puts "編集したいメモ内容の番号を入力してください："
+        edit_index = gets.to_i - 1 # 編集したいメモの番号を取得し、配列のインデックスに変換する
+      
+        if edit_index >= 0 && edit_index < memos.length
+          puts "現在の内容：#{memos[edit_index][0]}"
+          puts "新しい内容を入力してください："
+          new_content = gets.chomp # 新しい内容をユーザーから受け取る
+      
+          # メモを更新する
+          memos[edit_index][0] = new_content
+      
+          # 更新したメモをCSVファイルに書き込む
+          CSV.open("#{file_name}.csv", "w") do |csv|
+            memos.each do |memo|
+              csv << memo
+            end
+          end
+          puts "メモを編集しました"
+        else
+          puts "無効な番号です"
+        end
+      else
+        puts "値が不正です。"
       end
-    end
-    puts "メモを編集しました"
-  else
-    puts "無効な番号です"
-  end
-
-else
-  puts "値が不正です。"
-end
